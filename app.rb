b81c75317683a014c15ec80eaef8e5b06e9bfc11
Jess Martin <jessmartin@gmail.com>
2011-09-23 13:04:31 -0400

    Adding checking for images

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'sinatra/base'
 require File.join(File.dirname(__FILE__), 'posterizer', 'posterizer.rb')
 
 class App < Sinatra::Base
+  set :root, File.dirname(__FILE__)
+
   get '/' do
     index_html = File.read(File.join('public', 'index.html'))
     Posterizer.parse(index_html)
--- posterizer/posterizer.rb
+++ posterizer/posterizer.rb
@@ -5,7 +5,8 @@ class Posterizer
   def self.parse(html)
     settings = YAML::load(File.read('settings.yml'))
     settings.each do |key, value|
-      html.gsub!("{#{key}}", value)
+      #next if key == "Posts"
+      html.gsub!("{#{key}}", value.to_s)
     end
 
     html.gsub!("{PostViews}", (rand(100)+1).to_s)
